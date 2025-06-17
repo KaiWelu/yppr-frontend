337f2efeb5c1cada07c7a97e96445ab3de7bb3ac
KaiWelu <kaiweluda@gmail.com>
2025-06-18 00:15:08 +0200

    initial cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
 "use client";
-//import { useMovie } from "../app/hooks/useMovie";
-import InfoBanner from "./components/InfoBanner";
-import AboutGEMNews from "./components/ui/AboutGEMNews";
-import ArticleTile from "./components/ui/ArticleTile";
-import SupportProject from "./components/ui/SupportProject";
 
 export default function Home() {
   /* const { data, isPending, isFetching } = useMovie(); */
 
   /*  if (isPending) return <div>Is Loading!</div>; */
 
-  return (
-    <div>
-      <InfoBanner />
-      <ArticleTile />
-      <AboutGEMNews />
-      <SupportProject />
-    </div>
-  );
+  return <div>YOLO</div>;
 }
