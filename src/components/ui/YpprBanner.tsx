272d566a00513f77f3ffe4e6d1f3517fc16e05d1
KaiWelu <kaiweluda@gmail.com>
2025-06-18 17:47:51 +0200

    adds styling

--- src/components/ui/YpprBanner.tsx
+++ src/components/ui/YpprBanner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const YpprBanner = () => {
   return (
     <section className="flex flex-col items-center justify-center text-center w-full my-12 text-white gap-2 font-primary">
-      <h1 className="text-9xl font-bold drop-shadow-xl border-b-6 border-fuchsia-400 mb-2 hover:animate-pulse">
+      <h1 className="text-9xl font-bold drop-shadow-xl border-b-6 border-fuchsia-400 mb-2">
         YPPR
       </h1>
       <p className="text-3xl font-light drop-shadow-xl ">
