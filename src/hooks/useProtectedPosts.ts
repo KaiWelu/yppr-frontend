3933f508475c5d48587d26b7131fc8f5df135921
KaiWelu <kaiweluda@gmail.com>
2025-06-24 11:29:22 +0200

    refactors post hook

--- src/hooks/useProtectedPosts.ts
+++ src/hooks/useProtectedPosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import api from "@/api/axios";
 import { Post } from "@/types/Post";
 import { useAuth } from "@/context/authProvider";
@@ -12,12 +12,32 @@ const fetchProtectedPosts = async (token: string): Promise<Post[]> => {
   return response.data;
 };
 
+const createPost = async (newPost: Post, token: string) => {
+  const response = await api.post<Post>("/posts", newPost, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
 export const useProtectedPosts = () => {
   const { token } = useAuth(); // this gets the token from the context
 
   return useQuery({
-    queryKey: ["portected-posts"],
+    queryKey: ["protected-posts"],
     queryFn: () => fetchProtectedPosts(token),
     enabled: !!token, // only does a fetch if a token exists
+  });
+};
+
+export const useCreatePost = () => {
+  const { token } = useAuth(); // this gets the token from the context
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (newPost: Post) => createPost(newPost, token),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["protected-posts"] }); // this will invalidate and refetch after creation
+    },
   });
 };
